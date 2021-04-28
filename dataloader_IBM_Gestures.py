78a38f051e212e47933428aa649135c5c9cfa66e
Martino Sorbaro <martino.sorbaro@posteo.net>
2021-04-28 12:45:36 +0200

    fixed details in dataloader for gestures

--- dataloader_IBM_Gestures.py
+++ dataloader_IBM_Gestures.py
@@ -16,12 +16,13 @@ class IBMGesturesDataLoader:
         # - Create data directory if not exist
         self.path = pathlib.Path(__file__).parent.absolute() / "data" / "IBMGestures"
         self.path.mkdir(parents=True, exist_ok=True)
+        self.subfolder = "gestures_dataset_200ms"
 
         # - Download the data if not already exist
-        if not (self.path / "gestures_dataset_200ms").exists() :
-            p = str(self.path / "gestures_dataset_200ms.zip")
+        if not (self.path / self.subfolder).exists() :
+            p = str(self.path / f"{self.subfolder}.zip")
             os.system(f"wget {url} -O {p}")
-            with zipfile.ZipFile(self.path / "gestures_dataset_200ms.zip", 'r') as f:
+            with zipfile.ZipFile(self.path / f"{self.subfolder}.zip", 'r') as f:
                 f.extractall(self.path)
 
     def get_data_loader(self, dset, shuffle=True, num_workers=4, batch_size=128, dt=5000):
@@ -31,11 +32,11 @@ class IBMGesturesDataLoader:
         return dataloader
         """
         dataset = SpikeTrainDataset(
-            self.path / f"{dset}_DS/",
+            self.path / self.subfolder / dset,
             transform=np.float32,
-            force_n_bins=10,
             target_transform=int,
             dt=dt
         )
-        dataloader = DataLoader(dataset, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+        dataloader = DataLoader(dataset, shuffle=shuffle, num_workers=num_workers,
+                                batch_size=batch_size, drop_last=True)
         return dataloader
