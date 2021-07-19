d4b9c3236bb30e8bb1056cc8418bf4cbd0f90868
Martino Sorbaro <martino.sorbaro@posteo.net>
2021-07-19 15:03:45 +0200

    reviewed BMNIST dataloader and simplified round() function call

--- dataloader_BMNIST.py
+++ dataloader_BMNIST.py
@@ -8,8 +8,8 @@ import torchvision
 from torch.utils.data import DataLoader
 import os
 
-class BMNISTDataLoader:
 
+class BMNISTDataLoader:
     def __init__(
         self,
     ):
@@ -20,42 +20,38 @@ class BMNISTDataLoader:
             os.mkdir(self.path)
         if not (self.path / "B-MNIST/").exists():
             os.mkdir(self.path / "B-MNIST/")
-        
+
         p = self.path / "B-MNIST/"
         if not (self.path / "B-MNIST/MNIST.tar.gz").exists():
             os.system(f"wget -P {str(p)}  www.di.ens.fr/~lelarge/MNIST.tar.gz")
         if not (self.path / "B-MNIST/MNIST/").exists():
             os.system(f"tar -zxvf {str(p)}/MNIST.tar.gz -C {str(p)}/")
 
-        class RoundT:
-            def __call__(self,X):
-                return torch.round(X)
-            def __repr__(self):
-                return self.__class__.__name__ + '()'
+        # ToTensor turns the 0-255 uint representation into 0-1 floats
+        # torch.round rounds up or down at 0.5 as needed.
+        transforms = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torch.round
+        ])
 
         # - Download the data if not already exist
         self.mnist_train_ds = torchvision.datasets.MNIST(
             p,
-            train=True, 
+            train=True,
             download=False,
-            transform=torchvision.transforms.Compose([
-                torchvision.transforms.ToTensor(),
-                RoundT()])
+            transform=transforms
         )
 
         self.mnist_test_ds = torchvision.datasets.MNIST(
             p,
             train=False,
             download=False,
-            transform=torchvision.transforms.Compose([
-                torchvision.transforms.ToTensor(),
-                RoundT()])
+            transform=transforms
         )
-
 
     def get_data_loader(self, dset, shuffle=True, num_workers=4, batch_size=128):
         """
-        Get the torch dataloader 
+        Get the torch dataloader
         dset: "train" or "test"
         return dataloader
         """
@@ -64,5 +60,5 @@ class BMNISTDataLoader:
         elif dset == "test":
             dataloader = DataLoader(self.mnist_test_ds, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
         else:
-            assert dset in ["train","test"], "Unknown dset"
+            assert dset in ["train", "test"], "Unknown dset"
         return dataloader
