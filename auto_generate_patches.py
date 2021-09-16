074f664f0a85c5f4b64438cc6b2e2e46eebe242d
Yalun_Hu <yalun.hu@synsense.ai>
2021-09-16 14:05:17 +0800

    reform the loop order of hyper-param

--- auto_generate_patches.py
+++ auto_generate_patches.py
@@ -6,6 +6,6 @@ Generate adversarial patches by different hyper-param(epoch & target_label)
 
 
 if __name__ == "__main__":
-    for epoch in [1, 2, 3, 5, 7, 10]:
-        for label in range(2):
+    for label in range(6):
+        for epoch in [1, 3, 5, 10]:
             os.system(f"python dynapcnn_generate_patches.py {epoch} {label}")
