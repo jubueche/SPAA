3291b649af375525b77efbe65aa17d05511f3348
Gregor Lenz <mail@lenzgregor.com>
2021-09-22 18:51:24 +0100

    write summary of results to csv file

--- check_attack_success_rate.py
+++ check_attack_success_rate.py
@@ -1,11 +1,13 @@
 import numpy as np
 import os
 import re
+import csv
 
 
 if __name__ == "__main__":
     data_dir = "./attack_result_csv"
     csv_file_list = [each for each in os.listdir(data_dir) if each.endswith(".csv")]
+    results = []
     for csv_file in csv_file_list:
         numbers = re.findall(r"\d+\.?\d*", csv_file)
         epoch = int(numbers[0])
@@ -31,11 +33,12 @@ if __name__ == "__main__":
                     if sim_out_attacked_random == target_label: success_sample_random_sim += 1
 
                     total_sample += 1
-                success_rate_targeted_chip = success_sample_targeted_chip / total_sample
-                success_rate_random_chip = success_sample_random_chip / total_sample
-                success_rate_targeted_sim = success_sample_targeted_sim / total_sample
-                success_rate_random_sim = success_sample_random_sim / total_sample
-
+                success_rate_targeted_chip = round(success_sample_targeted_chip / total_sample, 4)
+                success_rate_random_chip = round(success_sample_random_chip / total_sample, 4)
+                success_rate_targeted_sim = round(success_sample_targeted_sim / total_sample, 4)
+                success_rate_random_sim = round(success_sample_random_sim / total_sample, 4)
+                
+                results.append((int(numbers[1]), success_rate_targeted_chip, success_rate_random_chip, success_rate_targeted_sim, success_rate_random_sim))
                 print(f"Epoch:{epoch}| Label: {target_label}"
                       f"| Target Patch Success Rate(on-chip): {success_rate_targeted_chip}"
                       f"| Random Patch Success Rate(on-chip): {success_rate_random_chip}"
@@ -43,3 +46,14 @@ if __name__ == "__main__":
                       f"| Random Patch Success Rate(Simulate):{success_rate_random_sim}\n")
         except:
             Warning("File still not valid")
+
+    f = open(data_dir+f'/ep{numbers[0]}_lbALL_num{numbers[2]}_patchsize{numbers[3]}.csv', 'w')
+    writer = csv.writer(f)
+
+    writer.writerow(("Label", "Target Patch Success Rate(on-chip)", "Random Patch Success Rate(on-chip)",\
+                     "Target Patch Success Rate(Simulate)", "Random Patch Success Rate(Simulate)"))
+    
+    sorted_by_label = sorted(results, key=lambda tup: tup[0])
+    for result in sorted_by_label:
+        writer.writerow(result)
+    f.close()
